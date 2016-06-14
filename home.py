ab428a3f2019406472f7191b237c3e81a08190a8
Scott Shuffler <scottshuffler@gmail.com>
2016-06-14 16:07:29 -0400

    Finished unoptimized product

--- home.py
+++ home.py
@@ -1,4 +1,18 @@
 import time 
+# the time you leave in military time including zeros and only numbers (163000)
+quitting_time_hour = 16
+quitting_time_minute = 30
+quitting_time_second = 00
+
 while True:
-    print("I want to go home.")
-    time.sleep(.5)
+    curr_time_hour = time.strftime("%H")
+    curr_time_minute = time.strftime("%M")
+    curr_time_second = time.strftime("%S")
+    if quitting_time_hour >= int(curr_time_hour):
+        diff_time_hours = quitting_time_hour - int(curr_time_hour)
+        diff_time_minutes = quitting_time_minute - int(curr_time_minute)
+        diff_time_seconds = 60 - int(curr_time_second)
+        print("You have " + str(diff_time_hours) + " hour(s) " + str(abs(diff_time_minutes)) + " minutes and " + str(abs(diff_time_seconds)) + " seconds remaining of work.")
+    else:
+        print("GO THE FUCK HOME")
+    time.sleep(1)
