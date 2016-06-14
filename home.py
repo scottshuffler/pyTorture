b118ca9d696d89993f683e4f70379f5b5c88ded3
Scott Shuffler <scottshuffler@gmail.com>
2016-06-14 16:36:03 -0400

    BUT WAIT THERES MORE

--- home.py
+++ home.py
@@ -8,7 +8,7 @@ while True:
     curr_time_hour = time.strftime("%H")
     curr_time_minute = time.strftime("%M")
     curr_time_second = time.strftime("%S")
-    if quitting_time_hour >= int(curr_time_hour):
+    if quitting_time_hour >= int(curr_time_hour) and quitting_time_minute >= int(curr_time_minute):
         diff_time_hours = quitting_time_hour - int(curr_time_hour)
         diff_time_minutes = quitting_time_minute - int(curr_time_minute)
         diff_time_seconds = 60 - int(curr_time_second)
