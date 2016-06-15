01b1768f2994131f7a52506a9b6269b542cbaabb
Scott Shuffler <scottshuffler@gmail.com>
2016-06-15 14:01:21 -0400

    Fixed the issue with times, it curses less, and sleeps when you're not working

--- home.py
+++ home.py
@@ -1,18 +1,32 @@
 import time 
 # the time you leave in military time including zeros and only numbers (163000)
-quitting_time_hour = 16
-quitting_time_minute = 30
-quitting_time_second = 00
+starting_time = 900
+quitting_time = 1630
+
 
 while True:
-    curr_time_hour = time.strftime("%H")
-    curr_time_minute = time.strftime("%M")
-    curr_time_second = time.strftime("%S")
-    if quitting_time_hour >= int(curr_time_hour) and quitting_time_minute >= int(curr_time_minute):
-        diff_time_hours = quitting_time_hour - int(curr_time_hour)
-        diff_time_minutes = quitting_time_minute - int(curr_time_minute)
-        diff_time_seconds = 60 - int(curr_time_second)
-        print("You have " + str(diff_time_hours) + " hour(s) " + str(abs(diff_time_minutes)) + " minutes and " + str(abs(diff_time_seconds)) + " seconds remaining of work.")
+    quitting_time_h = quitting_time / 100
+    quitting_time_m = quitting_time % 100
+
+    starting_time_h = starting_time / 100
+    starting_time_m = starting_time % 100
+
+    curr_time_hour = int(time.strftime("%H"))
+    curr_time_minute = int(time.strftime("%M"))
+    curr_time_second = int(time.strftime("%S"))
+
+    quitting_time_in_seconds = (quitting_time_h * 3600) + (quitting_time_m * 60)
+    starting_time_in_seconds = (starting_time_h * 3600) + (starting_time_m * 60)
+    current_time_in_seconds = (curr_time_hour * 60 * 60) + (curr_time_minute * 60) + curr_time_second
+
+    if quitting_time_in_seconds > current_time_in_seconds >= starting_time_in_seconds:
+        diff_time_in_seconds = quitting_time_in_seconds - current_time_in_seconds
+        remaining_time_in_seconds = diff_time_in_seconds % 60
+        remaining_time_in_minutes = int(((diff_time_in_seconds - remaining_time_in_seconds) / 60) % 60)
+        remaining_time_in_hours = int(((diff_time_in_seconds - (60 * remaining_time_in_minutes) - remaining_time_in_seconds) / 3600))
+
+        print("You have " + str(remaining_time_in_hours) + " hour(s) " + str(remaining_time_in_minutes) + " minutes and " + str(remaining_time_in_seconds) + " seconds remaining of work.")
+        time.sleep(1)
     else:
-        print("GO THE FUCK HOME")
-    time.sleep(1)
+        print("IT'S TIME TO GO HOME")
+        time.sleep(quitting_time_in_seconds - starting_time_in_seconds)
